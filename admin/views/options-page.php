34a5a7868c1cd39c354f83c0b3794474f949bdd4
Roel Magdaleno <roelmagdaleno@gmail.com>
2022-06-30 19:18:16 -0500

    IMPROVE: Add giscus links

--- admin/views/options-page.php
+++ admin/views/options-page.php
@@ -1,6 +1,14 @@
 <div class="wrap">
 	<h1>Giscus</h1>
 
+	<div class="giscus-links">
+		<a href="https://giscus.app/" target="_blank" style="margin-right: 15px;">Documentation</a>
+
+		<a href="https://github.com/giscus/giscus" target="_blank" style="margin-right: 15px;">GitHub: Giscus</a>
+
+		<a href="https://github.com/roelmagdaleno/giscus-for-wordpress" target="_blank">GitHub: Giscus for WordPress</a>
+	</div>
+
 	<form method="POST" action="options.php" novalidate>
 		<?php
 
